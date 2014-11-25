d66709395fddc426404f4122a6cde68163ce3364
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2014-11-25 22:08:57 +0100

    IOwinResponse extension test added

---
+++ src/LibOwin.Tests/IOwinResponseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Shouldly;
+
+namespace LibOwin.Owin 
+{
+    using Xunit;
+
+    public class IOwinResponseTests 
+    {
+        [Fact]
+        public void When_using_strongly_typed_OnSendingHeaders_it_should_be_invoked_at_sending_headers()
+        {
+            var owinResponse = new OwinResponse();
+            owinResponse.Environment["server.OnSendingHeaders"] = new Action<Action<object>, object>((action, obj) => action(obj));
+            owinResponse.OnSendingHeaders(str =>
+            {
+                str.ShouldBeTypeOf(typeof(string));
+                str.ShouldBe("Strongly typed!");
+            },"Strongly typed!");
+
+            
+        }
+    }
+}
